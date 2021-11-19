b9298c80caa0525b48115fb340676ba8644c68d5
Michal Pogoda <michalpogoda@hotmail.com>
2021-11-19 19:38:29 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="torpedo",
-    version="1.0.0",
+    version="1.0.1",
     author="Michał Pogoda",
     author_email="michal.pogoda@bards.ai",
     description="A simple library for using tor as proxy for requests",
